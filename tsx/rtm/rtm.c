3060469a255504d817ea4e1a67e9558de14b4465
João Paulo Labegalini de Carvalho <jaopaulolc@gmail.com>
2014-07-09 12:27:04 -0300

    removido geração da classificação dos aborts da librtm. agora os conflitos são capturados utilizando apenas a PCM.

--- tsx/rtm/rtm.c
+++ tsx/rtm/rtm.c
@@ -11,11 +11,8 @@
 #include <limits.h>
 #include <math.h>
 
-static tsx_tx_t *__global_thread_tx;
-
-static __thread tsx_tx_t *__thread_tx;
-
 static __thread long __tx_status;  // _xbegin() return status
+static __thread long __tx_id;  // tx thread id
 #define RTM_MAX_RETRIES 5
 static __thread long __tx_retries; // current number of retries
 
@@ -39,16 +36,7 @@ static lock_t __rtm_global_lock = LOCK_INITIALIZER;
 
 #define _XABORT_LOCKED 0xff
 
-static void __rtm_update_abort_stats(long status,long *retries,tsx_tx_t *tx);
-
-void TSX_START(long nthreads){
-
-	__global_thread_tx = (tsx_tx_t*)calloc(nthreads,sizeof(tsx_tx_t));
-	int i;
-	for(i=0; i < nthreads; i++){
-		__global_thread_tx[i].nthreads = nthreads;
-	}
-}
+static void __rtm_abort_update_retries(long status,long *retries);
 
 void TX_START(){
 	
@@ -64,8 +52,7 @@ void TX_START(){
 		else{
 			__tx_status = _XABORT_CODE(__tx_status);
 			// execution flow continues here on transaction abort
-			__tx_retries++;
-			__rtm_update_abort_stats(__tx_status,&__tx_retries,__thread_tx);
+			__rtm_abort_update_retries(__tx_status,&__tx_retries);
 		#ifdef RTM_CM_AUXLOCK
 			if(__aux_lock_owner == 0 && __tx_status == _XABORT_CONFLICT){
 				lock(&__aux_lock);
@@ -129,95 +116,37 @@ void TX_END(){
 
 void TX_INIT(long id){
 
-	if(__global_thread_tx != NULL){
-		__thread_tx = &__global_thread_tx[id];
-		__thread_tx->id              = id;
-		__tx_status									 = 0;
-		__tx_retries								 = 0;
-		#ifdef RTM_CM_TRYLOCK
-			//set transaction's sleep time to 3 nanoseconds
-			__tx_timespec.tv_sec = 0;
-			__tx_timespec.tv_nsec = 3;
-		#endif /* RTM_CM_TRYLOCK */
-		#ifdef RTM_CM_BACKOFF
-			unsigned short seed[3];
-			seed[0] = (unsigned short)rand()%USHRT_MAX;
-			seed[1] = (unsigned short)rand()%USHRT_MAX;
-			seed[2] = (unsigned short)rand()%USHRT_MAX;
-			__thread_seed = nrand48(seed);
-			__thread_backoff = MIN_BACKOFF;
-		#endif /* RTM_CM_BACKOFF */
-		__thread_tx->totalAborts     = 0;
-		__thread_tx->explicitAborts  = 0;
-		__thread_tx->conflictAborts  = 0;
-		__thread_tx->capacityAborts  = 0;
-		__thread_tx->debugAborts     = 0;
-		__thread_tx->nestedAborts    = 0;
-		__thread_tx->unknownAborts   = 0;
-	}
+	__tx_id      = id;
+	__tx_status  = 0;
+	__tx_retries = 0;
+	#ifdef RTM_CM_TRYLOCK
+		//set transaction's sleep time to 3 nanoseconds
+		__tx_timespec.tv_sec = 0;
+		__tx_timespec.tv_nsec = 3;
+	#endif /* RTM_CM_TRYLOCK */
+	#ifdef RTM_CM_BACKOFF
+		unsigned short seed[3];
+		seed[0] = (unsigned short)rand()%USHRT_MAX;
+		seed[1] = (unsigned short)rand()%USHRT_MAX;
+		seed[2] = (unsigned short)rand()%USHRT_MAX;
+		__thread_seed = nrand48(seed);
+		__thread_backoff = MIN_BACKOFF;
+	#endif /* RTM_CM_BACKOFF */
 }
 
-void TSX_FINISH(){
-
-	#ifdef RTM_ABORT_DEBUG
-		long totalAborts    = 0;
-		long explicitAborts = 0;
-		long conflictAborts = 0;
-		long capacityAborts = 0;
-		long debugAborts    = 0;
-		long nestedAborts   = 0;
-		long unknownAborts  = 0;
-		long nthreads = __global_thread_tx[0].nthreads;
-		long i;
-		printf("Core total explicit conflict capacity debug nested unknown\n");
-		for(i=0; i < nthreads; i++){
-			tsx_tx_t *tx = __global_thread_tx;
-			printf(" %ld %ld\t%ld\t%ld\t%ld\t%ld\t%ld\t%ld\n",
-				tx[i].id,tx[i].totalAborts,
-				tx[i].explicitAborts,tx[i].conflictAborts,
-				tx[i].capacityAborts,tx[i].debugAborts,
-				tx[i].nestedAborts,tx[i].unknownAborts);
-			
-			totalAborts    += tx[i].totalAborts;
-			explicitAborts += tx[i].explicitAborts;
-			conflictAborts += tx[i].conflictAborts;
-			capacityAborts += tx[i].capacityAborts;
-			debugAborts    += tx[i].debugAborts;
-			nestedAborts   += tx[i].nestedAborts;
-			unknownAborts  += tx[i].unknownAborts;
-		}
-		printf("----------------------------------------------------------\n");
-		printf(" * %ld\t%ld\t%ld\t%ld\t%ld\t%ld\t%ld\n",
-			totalAborts,explicitAborts,
-			conflictAborts,capacityAborts,
-			debugAborts,nestedAborts,unknownAborts);
-		free(__global_thread_tx);
-	#endif /* RTM_ABORT_DEBUG */
-}
-
-static void __rtm_update_abort_stats(long status,long *retries,tsx_tx_t *tx){
+static void __rtm_abort_update_retries(long status, long *retries){
 	
-	tx->totalAborts++;
 	switch(status){
 		case _XABORT_EXPLICIT:
-			tx->explicitAborts++;
-			return;
 		case _XABORT_CONFLICT:
-			tx->conflictAborts++;
-			return;
+			(*retries)++;
+			break;
 		case _XABORT_CAPACITY: /* ++ */
-			tx->capacityAborts++;
-			break;
 		case _XABORT_DEBUG:    /* ++ */
-			tx->debugAborts++;
-			break;
 		case _XABORT_NESTED:   /* ++ */
-			tx->nestedAborts++;
-			break;
 		default:
-			tx->unknownAborts++;
-			return;
+		//++ transaction will not commit on future attempts
+			(*retries) = RTM_MAX_RETRIES;
+			break;
 	}
-	//++ transaction will not commit on future attempts
-	(*retries) = RTM_MAX_RETRIES;
 }
--- tsx/rtm/rtm.h
+++ tsx/rtm/rtm.h
@@ -6,20 +6,6 @@
 /* Requires TSX support in the CPU */
 #include <immintrin.h>
 
-typedef struct _tsx_tx_t{
-	long id; 								// thread id
-	long nthreads;
-	long totalAborts; 			// total number of aborts
-#ifdef RTM_ABORT_DEBUG
-	long explicitAborts;		// aborts due to _xabort() call
-	long conflictAborts;		// aborts due to data conflict
-	long capacityAborts;		// aborts due to L1 overflow
-	long debugAborts;				// aborts due to debug trap
-	long nestedAborts;			// aborts due to inner nested transaction abort
-	long unknownAborts; 		// aborts whose cause is unknown
-#endif  /* RTM_ABORT_DEBUG */
-}tsx_tx_t;
-
 void TX_START(); 
 void TX_END(); 
 void TX_INIT(long id); 
