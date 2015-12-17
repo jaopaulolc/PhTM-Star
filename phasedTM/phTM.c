de7d54e78b099fe46ecb22886f27279c4ab538d1
jaopaulolc <jaopaulolc@gmail.com>
2015-12-17 11:37:57 -0200

    phasedTM/phTM.c   + adicionado contadores de timestamp para as transições e um contador   de transições HW->SW causados por cancelamentos de capacidade scripts/compile   + opção -P para habilitar o perfilamento das transições scripts/execute   + suporte para armazenar os dados do perfilamento das transições   + pequena correções no 'case' das builds (\? -> *)

--- phasedTM/phTM.c
+++ phasedTM/phTM.c
@@ -21,8 +21,27 @@ modeIndicator_t modeIndicator	= { .mode = HW,
 
 #include <utils.h>
 
+#ifdef PHASE_PROFILING
+#include <sys/time.h>
+#define MAX_TRANS 1000000
+static uint64_t start_time __ALIGN__ = 0;
+static uint64_t trans_index __ALIGN__ = 1;
+static uint64_t capacity_abort_transitions __ALIGN__ = 0;
+static uint64_t hw_sw_transitions __ALIGN__ = 0;
+static uint64_t sw_hw_transitions __ALIGN__ = 0;
+static uint64_t trans_timestamp[MAX_TRANS] __ALIGN__;
+
+static uint64_t getTime(){
+	struct timespec t;
+	clock_gettime(CLOCK_MONOTONIC, &t);
+	return (uint64_t)(t.tv_sec*1.0e9) + (uint64_t)(t.tv_nsec);
+}
+
+#endif /* PHASE_PROFILING */
+
+static
 void
-changeMode(mode_t newMode) {
+changeMode(mode_t newMode, uint32_t htm_abort_reason) {
 	
 	bool success;
 	modeIndicator_t indicator;
@@ -43,6 +62,13 @@ changeMode(mode_t newMode) {
 			new = setMode(indicator, SW);
 			success = boolCAS(&(modeIndicator.value), &(expected.value), new.value);
 		} while (!success && (indicator.mode != SW));
+#ifdef PHASE_PROFILING
+		if(success){
+			trans_timestamp[trans_index++] = getTime() - start_time;
+			hw_sw_transitions++;
+			if(htm_abort_reason & ABORT_CAPACITY) capacity_abort_transitions++; 
+		}
+#endif /* PHASE_PROFILING */
 	} else {
 		do {
 			indicator = atomicReadModeIndicator();
@@ -50,6 +76,12 @@ changeMode(mode_t newMode) {
 			new = setMode(NULL_INDICATOR, HW);
 			success = boolCAS(&(modeIndicator.value), &(expected.value), new.value);
 		} while (!success && (indicator.mode != HW));
+#ifdef PHASE_PROFILING
+		if(success){
+			trans_timestamp[trans_index++] = getTime() - start_time;
+			sw_hw_transitions++;
+		}
+#endif /* PHASE_PROFILING */
 	}
 }
 
@@ -80,7 +112,7 @@ HTM_Start_Tx() {
 		
 		htm_retries++;
 		if (htm_retries >= HTM_MAX_RETRIES || abort_reason & ABORT_CAPACITY) {
-			changeMode(SW);
+			changeMode(SW, abort_reason);
 			return true;
 		}
 	}
@@ -108,7 +140,7 @@ STM_PreStart_Tx(bool restarted) {
 			indicator = atomicReadModeIndicator();
 			if (indicator.deferredCount == 0 || indicator.mode == HW) {
 				if(restarted) atomicDecUndeferredCount();
-				changeMode(HW);
+				changeMode(HW, 0);
 				return true;
 			}
 			if(restarted) break;
@@ -143,7 +175,7 @@ STM_PostCommit_Tx() {
 	if (deferredTx){
 		deferredTx = false;
 		if (new.deferredCount == 0) {
-			changeMode(HW);
+			changeMode(HW, 0);
 		}
 	}
 }
@@ -156,9 +188,34 @@ phTM_init(long nThreads){
 void
 phTM_thread_init(long tid){
 	__tx_tid = tid;
+#ifdef PHASE_PROFILING
+	start_time = getTime();
+#endif /* PHASE_PROFILING */
 }
 
 void
 phTM_term(long nThreads, long nTxs, unsigned int **stmCommits, unsigned int **stmAborts){
 	__term_prof_counters(nThreads, nTxs, stmCommits, stmAborts);
+
+#ifdef PHASE_PROFILING
+	
+	FILE *f = fopen("transitions.timestamp", "w");
+	if(f == NULL){
+		perror("fopen");
+	}
+
+	printf("hw->sw: %lu\n", hw_sw_transitions);
+	printf("sw->hw: %lu\n", sw_hw_transitions);
+	printf("capacity_abort_transations: %lu\n", capacity_abort_transitions);
+	
+	trans_timestamp[0] = 0;
+
+	uint64_t i;
+	for (i=1; i < trans_index; i++){
+		uint64_t dx = trans_timestamp[i] - trans_timestamp[i-1];
+		fprintf(f, "%lu %d\n", dx, i % 2 == 1);
+	}
+	fprintf(f, "\n\n");
+	fclose(f);
+#endif /* PHASE_PROFILING */
 }
